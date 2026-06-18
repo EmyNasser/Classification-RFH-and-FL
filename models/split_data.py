5f89eabc080b7e800c8a9e5ddd672e13dc81ad2f
EmyNasser <129627251+EmyNasser@users.noreply.github.com>
2026-06-18 12:54:51 +0200

    d

--- models/split_data.py
+++ models/split_data.py
@@ -2,42 +2,56 @@ from sklearn.model_selection import train_test_split
 import os
 import shutil
 
-src = "/kaggle/input/datasets/eman12345nasser/ucmerced-landuse"
-
+src = "/kaggle/input/datasets/eman12345nasser/ucmerced-landuse/Images"
 output = "/kaggle/working/data"
 
-for split in ["train","val","test"]:
-    os.makedirs(os.path.join(output,split),exist_ok=True)
+for split in ["train", "val", "test"]:
+    os.makedirs(os.path.join(output, split), exist_ok=True)
 
 for cls in os.listdir(src):
 
-    cls_path = os.path.join(src,cls)
+    cls_path = os.path.join(src, cls)
 
     if not os.path.isdir(cls_path):
         continue
 
-    imgs = [os.path.join(cls_path,x) for x in os.listdir(cls_path)]
+    imgs = [
+        os.path.join(cls_path, img)
+        for img in os.listdir(cls_path)
+        if img.lower().endswith(
+            (".jpg", ".jpeg", ".png", ".tif", ".tiff")
+        )
+    ]
 
-    train,valtest = train_test_split(
+    train_imgs, temp_imgs = train_test_split(
         imgs,
-        test_size=0.3,
+        test_size=0.30,
         random_state=42
     )
 
-    val,test = train_test_split(
-        valtest,
-        test_size=0.5,
+    val_imgs, test_imgs = train_test_split(
+        temp_imgs,
+        test_size=0.50,
         random_state=42
     )
 
-    for split,data in zip(
-        ["train","val","test"],
-        [train,val,test]
-    ):
+    splits = {
+        "train": train_imgs,
+        "val": val_imgs,
+        "test": test_imgs
+    }
 
-        save_dir = os.path.join(output,split,cls)
+    for split_name, split_imgs in splits.items():
 
-        os.makedirs(save_dir,exist_ok=True)
+        save_dir = os.path.join(
+            output,
+            split_name,
+            cls
+        )
 
-        for img in data:
-            shutil.copy(img,save_dir)
+        os.makedirs(save_dir, exist_ok=True)
+
+        for img_path in split_imgs:
+            shutil.copy2(img_path, save_dir)
+
+print("Done.")
